8c6b7264cd58875f8956edbd14b21a9661b2ffe4
yuto <yuto.nozaki1997@gmail.com>
2023-06-30 21:36:17 +0900

    username削除

--- app/management/commands/superuser.py
+++ app/management/commands/superuser.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
             User.objects.create_superuser(
-                username=settings.SUPERUSER_NAME,
+                # username=settings.SUPERUSER_NAME,
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD
             )
